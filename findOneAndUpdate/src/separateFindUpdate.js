3207e6e1014f8a141030b0ba48f83d32f6c5a530
kirkdotcam <camden.kirkland@gmail.com>
2023-05-04 14:04:01 -0500

    add findOneAndUpdate example. Need to clean up

---
+++ findOneAndUpdate/src/separateFindUpdate.js
@@ -0,0 +1,71 @@
+const {MongoClient} = require("mongodb");
+
+const mongouri = "mongodb+srv://camkirk:bananabanana@cluster0.3zt9gfn.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(mongouri);
+const collection = client.db("test").collection("payloads");
+
+const cluster = require("cluster");
+const process = require("process");
+const numCPUs = require("os").cpus().length;
+
+const documentKeys = {
+    ID: "_id"
+}
+
+async function run() {
+    
+    const queryDoc = {
+        [documentKeys.ID]:"testDoc"
+    };
+    
+    const mutationDoc = {
+        $inc:{
+            value:1,
+            timesAccessed:1
+        }};
+    const optionDoc = {upsert: true};
+
+    
+    
+    const testDoc = await collection.findOne(queryDoc);
+
+    await collection.updateOne(queryDoc, mutationDoc,optionDoc );
+
+    const postMutationDoc = await collection.findOne(queryDoc);
+
+    
+    
+    const expectedValue = testDoc.value + 1;
+    const actualValue = postMutationDoc.value;
+
+    if (expectedValue != actualValue) {
+        
+        console.log(`Mismatch: Expected ${expectedValue}, got ${actualValue}`);
+    }
+
+
+}
+
+if (cluster.isPrimary) {
+    console.log(`primary: ${process.pid}`);
+
+    for (let i = 0; i < numCPUs/2; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+
+} else {
+    console.log(`starting ${process.pid}`);
+
+    let loops = 0;
+    while (loops < 10){
+        run();
+        loops++;
+    }
+
+    // process.exit();
+}
