7ae2e3a45130ca0741221b95e15d6103f732ae50
kirkdotcam <camden.kirkland@gmail.com>
2023-11-08 08:11:27 -0600

    add remote version of generateGraph

---
+++ generateGraph/remote.js
@@ -0,0 +1,129 @@
+const allParents = []
+const allChildren = []
+let nodeCounter = 0
+let NUMPARENTS = 10_000
+let NUMCHILDREN = 80_000
+
+const { MongoClient} = require('mongodb');
+const MONGODBURI = "mongodb://localhost:27017";
+
+const client = new MongoClient(MONGODBURI)
+
+
+
+class GraphNode{
+    constructor({name="node", status="ongoing", ancestors=[], parentName=null, type=null}){
+
+        this.name = name;
+        this.ancestors = ancestors;
+        this.parentName = parentName;
+        this.status = status;
+        this.type = type;
+        nodeCounter++
+    }
+
+    addAncestor(nodeName){
+        if (typeof nodeName !== 'number') throw "adding non-string ancestor is not allowed"
+
+        this.ancestors.push(nodeName)
+
+    }
+
+
+}
+
+
+
+function randomStatus(){
+    let statusValues = ["ongoing", "complete", "canceled"]
+    return statusValues[Math.floor(Math.random()*statusValues.length)]
+}
+
+function createParent(name, status, allParents){
+    if (status === null || status === undefined) status = randomStatus();
+    if (name === null || name === undefined) name = "node"
+
+    let node = new GraphNode({name: name, status: status, type: "parent"})
+    allParents.push(node)
+    return node
+}
+
+function findParentByName(parentName){
+    if (parentName > allParents.length -1) return null
+    return allParents.filter((parent) => parent.name === parentName)[0]
+   
+}
+
+function createChild(name, parentName, ancestors){
+    //if no parent, find a random parent
+    if (parentName === null || parentName === undefined){
+        parentName = Math.floor(Math.random()*allParents.length)
+    }
+
+    //grabParent ref
+    let parentNode = findParentByName(parentName)
+
+    //
+    let node = new GraphNode({
+        name: name, 
+        status: parentNode.status, 
+        parentName: parentName, 
+        ancestors: [parentName],
+        type: "child"
+    })
+    
+    
+    let randomAncestor = findRandomChild(parentName)
+    if (randomAncestor === undefined) randomAncestor = parentNode
+    
+    if (randomAncestor.type === "child"){
+        node.ancestors = Array.from(new Set(node.ancestors.concat(randomAncestor.ancestors)))
+    }
+
+    allChildren.push(node)
+    nodeCounter++
+
+}
+
+function findRandomChild(parentName){
+
+    if (parentName){
+        let children = allChildren
+            .filter((childNode)=> childNode.parentName === parentName)
+        
+        return children[Math.floor(Math.random()*children.length)]
+
+    } else {
+
+        
+        return allChildren[Math.floor(Math.random()*allChildren.length)]
+    }
+}
+
+async function run(){
+
+    await client.connect()
+    const jobsCollection = await client.db("test").collection("jobs")
+    try  {
+        await jobsCollection.drop()
+
+    } catch (e){
+        console.log("no collection to drop")
+    }
+    
+    while (allParents.length < NUMPARENTS){
+        createParent(nodeCounter, randomStatus(), allParents)
+    }
+    
+    while ((nodeCounter - NUMPARENTS) < NUMCHILDREN){
+        createChild(nodeCounter)
+    }
+
+    await jobsCollection.insertMany(allParents)
+    await jobsCollection.insertMany(allChildren)
+    
+    console.log(await jobsCollection.countDocuments())
+    process.exit()
+}
+
+run()
