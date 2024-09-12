68155a977c1bd2be2989654a575f4a349955322b
kirkdotcam <camden.kirkland@gmail.com>
2024-09-12 12:11:20 -0500

    update chStreamFlood with reflector and index

--- chStreamFlood/reader.js
+++ chStreamFlood/reader.js
@@ -1,6 +1,7 @@
-const {MongoClient} = require("mongodb");
+const {MongoClient} = require("mongodb")
+// import {MongoClient} from "mongodb";
 
-const mongouri = "mongodb+srv://camkirk:bananabanana@cluster0.0ejrueo.mongodb.net/?retryWrites=true&w=majority";
+const mongouri = "mongodb+srv://camkirk:bananabanana@cluster0.qdgica8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(mongouri);
 const collection = client.db("test").collection("payloads");
--- chStreamFlood/writer.js
+++ chStreamFlood/writer.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require("mongodb");
+// import {MongoClient} from "mongodb";
 
-const mongouri = "mongodb+srv://camkirk:bananabanana@cluster0.sn4vd2e.mongodb.net/?retryWrites=true&w=1";
+const mongouri = "mongodb+srv://camkirk:bananabanana@cluster0.qdgica8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(mongouri);
 const collection = client.db("test").collection("payloads");
