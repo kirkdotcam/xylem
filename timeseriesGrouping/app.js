80e1409b49ad182bf050d53e4817446b4b4a32c8
kirkdotcam <camden.kirkland@gmail.com>
2025-12-14 22:22:46 -0600

    user created or updated on login

--- timeseriesGrouping/app.js
+++ timeseriesGrouping/app.js
@@ -1,4 +1,4 @@
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 const mongourl = "mongodb://localhost:27017"
 const client = new MongoClient(mongourl)
 
@@ -7,39 +7,45 @@ const priceDataColl = timeDataDB.collection("priceData")
 
 //write a lot of data
 //
-let companies = ["NTNX", "AEMD", "BLEU", "PURP"]
-let stockType = ["preferred", "common"]
+let companies = ["GRN", "REHD", "BLEU", "PURP"]
 
-for (let rec = 0; rec < 1_000_000; rec++){
-  
-  let randomCompany = companies[Math.floor(Math.random() * companies.length)]
+let randomPrice = Math.random() * 20 + 20;
 
-  let randomPrice = Math.random()*20 + 20;
-  let randomType = stockType[Math.floor(Math.random()*stockType.length)]
-  
-  db.priceData.insertOne({
-    timestamp: new Date(),
-    ticker: randomCompany,
-    price: randomPrice,
-    stockType: randomType 
-  })
+for (let rec = 0; rec < 500_000; rec++) {
+
+
+  for (company of companies) {
+
+    randomReturn = (Math.random() * .1 + 0.5)
+
+    randomPrice = randomPrice * (1 + randomReturn)
+
+    console.log(randomPrice)
+    priceDataColl.insertOne({
+      timestamp: new Date(),
+      ticker: company,
+      price: randomPrice,
+    })
+  }
+
 
 }
 
 
 //query
 //
-db.priceData.aggregate([{
-  $match: {
-    $and: [{
-      "timestamp": {$gte: ISODate("2024-06-18T15:52:35.106Z")}
-    }]
-  }},
-  {
-  $project: {
-    _id: 0,
-    date: { minutes: {$minute: "$timestamp"} ,seconds: {$second: "$timestamp"}},
-    totalCount: {$sum: "$price" }
-  }
-}
-])
+// db.priceData.aggregate([{
+//   $match: {
+//     $and: [{
+//       "timestamp": { $gte: ISODate("2024-06-18T15:52:35.106Z") }
+//     }]
+//   }
+// },
+// {
+//   $project: {
+//     _id: 0,
+//     date: { minutes: { $minute: "$timestamp" }, seconds: { $second: "$timestamp" } },
+//     totalCount: { $sum: "$price" }
+//   }
+// }
+// ])
